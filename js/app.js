24780f13fb0b8a99851d5e6d8b852ba4a259a5e8
Jasmina Kruhak <jkruhak@gmail.com>
2014-07-13 19:43:21 +0400

    'x' action

--- js/app.js
+++ js/app.js
@@ -1,39 +1,57 @@
 $(document).ready(function() {
 
+	
 	$('.ryu').mouseenter(function() {  
 		$('.ryu-still').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-ready').show();
 	})
 
 	.mouseleave(function() { 
 		$('.ryu-ready').hide();
+		$('.ryu-cool').hide();
 		$('.ryu-still').show();
 	})
 
 	.mousedown(function() {
-	//console.log('mousedown');
-	//play hadouken sound
-	playHadouken();
-	//show hadouken 
-	$('.ryu-ready').hide();
-	$('.ryu-throwing').show();
-	$('.hadouken').finish().show()
-	//animate hadouken to the right of the screen
-	.animate(
-	  {'left': '300px'},
-	  500,
-	  function() {
-	    $(this).hide();
-	    $(this).css('left', '-212px');
-	  }
-	);
+		//play hadouken sound
+		playHadouken();
+		//show hadouken and animate it to the right of the screen
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show()
+		//animate hadouken to the right of the screen
+		.animate(
+		  {'left': '300px'},
+		  500,
+		  function() {
+		    $(this).hide();
+		    $(this).css('left', '-212px');
+		  }
+		);
 	})
 
 	.mouseup(function() {
-	//console.log('mouseup');
-	//ryu goes back to his ready position
-	$('.ryu-throwing').hide();
-	$('.ryu-ready').show();
+		//ryu goes back to his ready position
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
+	});
+
+	$('#section1').focus(function() {
+		$('#section1').keydown(function(event) { 
+			if (event.which == '88') {
+				$('.ryu-still').hide();
+				$('.ryu-ready').hide();
+				$('.ryu-cool').show();	
+			} 
+		});
+
+		$('#section1').keyup(function(event) {
+			if (event.which == '88') {
+				$('.ryu-cool').hide();
+				$('.ryu-still').show();
+			}
+		});
 	});
 
 	function playHadouken() {
